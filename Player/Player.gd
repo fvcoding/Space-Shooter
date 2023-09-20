674e05331f973a466dffd09d2ce21a693bc18acc
fvcoding <143767864+fvcoding@users.noreply.github.com>
2023-09-19 21:30:49 -0400

    Completed Project2 Part 3

--- Player/Player.gd
+++ Player/Player.gd
@@ -4,7 +4,10 @@ var speed = 5
 var rotate_speed = 0.08
 var max_speed = 400
 var nose = Vector2(0,-60)
+var health = 10
 var Bullet = load("res://Player/bullet.tscn")
+var Effects = null
+var Explosion = load("res://Effects/explosion.tscn")
 
 func get_input():
 	var to_return = Vector2.ZERO
@@ -39,8 +42,20 @@ func _physics_process(_delta):
 			Effects.add_child(bullet)
 			
 			
-	
+func damage(d):
+	health -= d
+	if health <= 0:
+		Effects = get_node_or_null("/root/Game/Effects")
+	if Effects != null:
+		var explosion = Explosion.instantiate()
+		Effects.add_child(explosion)
+		explosion.global_position = global_position
+		hide()
+		await explosion.animation_finished
+	queue_free()
 
 	
 
-
+func _on_area_2d_body_entered(body):
+	if body.name != "Player":
+		damage(100)
